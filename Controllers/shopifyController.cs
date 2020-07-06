caaf3e664562528878141373b998667864d3757b
ntello <team@unarix.com.ar>
2020-07-06 16:51:31 -0300

    add: shopify price and variants

--- Controllers/shopifyController.cs
+++ Controllers/shopifyController.cs
@@ -51,7 +51,7 @@ namespace a2klab.Controllers
             //{                 
                 var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(3600));
 
-                var client = new RestClient("https://ezelab.myshopify.com/admin/api/2020-10/products.json?fields=id,images,title");
+                var client = new RestClient("https://ezelab.myshopify.com/admin/api/2020-10/products.json?fields=id,images,title,handle,variants");
                 client.Timeout = -1;
                 var request = new RestRequest(Method.GET);
                 request.AddHeader("Authorization", "Basic Y2Y5Yjc1MjQ5YjkzMDhiODdkOGIyNmI4OGM2NzEzYTA6c2hwcGFfMDkxMWNkODBhMzYzMmQ5MzEyODE5MTM5ZDJiYTkzOWY=");
@@ -59,6 +59,11 @@ namespace a2klab.Controllers
                 IRestResponse response = client.Execute(request);
                 
                 products = JsonConvert.DeserializeObject<Root>(response.Content);
+
+                foreach(Product p in products.products)
+                {
+                    p.title = p.title + " - $" + p.variants[0].price;
+                }
 
                 memoryCache.Set("products", products, cacheEntryOptions);  
             //}  
@@ -90,11 +95,42 @@ namespace a2klab.Controllers
 
     }
 
+    public class Variant    {
+        public object id { get; set; } 
+        public object product_id { get; set; } 
+        public string title { get; set; } 
+        public string price { get; set; } 
+        public string sku { get; set; } 
+        public int position { get; set; } 
+        public string inventory_policy { get; set; } 
+        public string compare_at_price { get; set; } 
+        public string fulfillment_service { get; set; } 
+        public string inventory_management { get; set; } 
+        public string option1 { get; set; } 
+        public string option2 { get; set; } 
+        public string option3 { get; set; } 
+        public DateTime created_at { get; set; } 
+        public DateTime updated_at { get; set; } 
+        public bool taxable { get; set; } 
+        public string barcode { get; set; } 
+        public int grams { get; set; } 
+        public object image_id { get; set; } 
+        public double weight { get; set; } 
+        public string weight_unit { get; set; } 
+        public object inventory_item_id { get; set; } 
+        public int inventory_quantity { get; set; } 
+        public int old_inventory_quantity { get; set; } 
+        public bool requires_shipping { get; set; } 
+        public string admin_graphql_api_id { get; set; } 
+
+    }
+
     public class Product    {
         public object id { get; set; } 
         public string title { get; set; } 
+        public string handle { get; set; } 
+        public List<Variant> variants { get; set; } 
         public List<image> images { get; set; } 
-
     }
 
     public class Root    {
