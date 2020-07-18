ff0cf1679299a3c7db1efdb500ee1f2c32ff2c1d
ntello <team@unarix.com.ar>
2020-07-18 02:30:54 -0300

    clean

--- Controllers/FlightController.cs
+++ Controllers/FlightController.cs
@@ -111,51 +111,50 @@ namespace a2klab.Controllers
             twilio.actions = actions;
             return twilio;
         }
-  
     }
+}
+
+
+public class Flight    {
+    public string id { get; set; } 
+    public string stda { get; set; } 
+    public string arpt { get; set; } 
+    public string idaerolinea { get; set; } 
+    public string aerolinea { get; set; } 
+    public string mov { get; set; } 
+    public string nro { get; set; } 
+    public string logo { get; set; } 
+    public string destorig { get; set; } 
+    public string IATAdestorig { get; set; } 
+    public string etda { get; set; } 
+    public string atda { get; set; } 
+    public string sector { get; set; } 
+    public string termsec { get; set; } 
+    public string gate { get; set; } 
+    public string estes { get; set; } 
+    public string estin { get; set; } 
+    public string estbr { get; set; } 
+    public string color { get; set; } 
+    public string matricula { get; set; } 
+    public object chk_from { get; set; } 
+    public object chk_to { get; set; } 
+    public string belt { get; set; } 
+    public object chk_lyf { get; set; } 
+    public object sdtempunit { get; set; } 
+    public object sdtemp { get; set; } 
+    public object sdphrase { get; set; } 
+    public object idclimaicono { get; set; } 
+    public object tipoVuelo { get; set; } 
+    public object idshared { get; set; } 
+    public object acftype { get; set; } 
+    public object pasajeros { get; set; } 
+    public object posicion { get; set; } 
+    public object term { get; set; } 
 
 }
-// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
-    public class Flight    {
-        public string id { get; set; } 
-        public string stda { get; set; } 
-        public string arpt { get; set; } 
-        public string idaerolinea { get; set; } 
-        public string aerolinea { get; set; } 
-        public string mov { get; set; } 
-        public string nro { get; set; } 
-        public string logo { get; set; } 
-        public string destorig { get; set; } 
-        public string IATAdestorig { get; set; } 
-        public string etda { get; set; } 
-        public string atda { get; set; } 
-        public string sector { get; set; } 
-        public string termsec { get; set; } 
-        public string gate { get; set; } 
-        public string estes { get; set; } 
-        public string estin { get; set; } 
-        public string estbr { get; set; } 
-        public string color { get; set; } 
-        public string matricula { get; set; } 
-        public object chk_from { get; set; } 
-        public object chk_to { get; set; } 
-        public string belt { get; set; } 
-        public object chk_lyf { get; set; } 
-        public object sdtempunit { get; set; } 
-        public object sdtemp { get; set; } 
-        public object sdphrase { get; set; } 
-        public object idclimaicono { get; set; } 
-        public object tipoVuelo { get; set; } 
-        public object idshared { get; set; } 
-        public object acftype { get; set; } 
-        public object pasajeros { get; set; } 
-        public object posicion { get; set; } 
-        public object term { get; set; } 
 
-    }
+public class Flights {
+    public List<Flight> MyFlight { get; set; } 
 
-    public class Flights {
-        public List<Flight> MyFlight { get; set; } 
-
-    }
+}
 
